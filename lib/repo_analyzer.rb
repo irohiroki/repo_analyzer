b62d8d957f54453ccf006683652d96197040fd4c
Hiroki Yoshioka <irohiroki@gmail.com>
2013-03-20 19:13:18 +0900

    connect repository model to document

--- lib/repo_analyzer.rb
+++ lib/repo_analyzer.rb
@@ -1,7 +1,12 @@
 require "repo_analyzer/version"
 
-require 'irby'
+require 'irby' unless $0 == 'irb'
 require 'mongoid'
+
+require 'models/repository'
+
+ENV['MONGOID_ENV'] ||= 'development'
+Mongoid.load!(File.expand_path('../../config/mongoid.yml', __FILE__))
 
 module RepoAnalyzer
   # Your code goes here...
