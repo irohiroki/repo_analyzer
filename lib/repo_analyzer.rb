041fdb36f9db2465c064f249627a1eabbcbeb4d8
Hiroki Yoshioka <irohiroki@gmail.com>
2013-03-23 14:12:25 +0900

    implement incremental fetch

--- lib/repo_analyzer.rb
+++ lib/repo_analyzer.rb
@@ -29,5 +29,14 @@ module RepoAnalyzer
         Repository.create repo
       end
     end
+
+    def fetch_upto(id_end)
+      loop do
+        last = Repository.last
+        break if last.github_id >= id_end
+        $stderr.puts "fetch since #{last.github_id}..."
+        fetch since: last.github_id
+      end
+    end
   end
 end
