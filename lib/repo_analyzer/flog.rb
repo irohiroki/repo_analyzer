fac1a0fd95672c91734b2a6e73afb0871833b0c8
Hiroki Yoshioka <irohiroki@gmail.com>
2013-04-29 11:57:24 +0900

    more stable flog measurement

--- lib/repo_analyzer/flog.rb
+++ lib/repo_analyzer/flog.rb
@@ -1,19 +1,33 @@
 module RepoAnalyzer
   class Flog
     def self.measure(root)
-      IO.popen("cd #{root} && flog lib") do |f|
-        f.gets # discard total line
+      IO.popen("cd #{root} && flog lib 2>&1") do |f|
+        data = {}
+
+        loop do
+          case line = f.gets
+          when /flog total/
+            break
+          when /warning/
+            data = {flog_warned: true}
+          when nil
+            return {skipped: 'flog'}
+          else
+            print line
+          end
+        end
+
         average = parse_point(f.gets)
         raise 'No code to measure' if average.zero?
         f.gets # discard blank line
         worst_line = f.gets
 
-        {
+        data.merge(
           flog_average: average,
           flog_worst: parse_point(worst_line),
           flog_worst_method: worst_line[/(?<=: ).*/],
           flog_measured_at: Time.now.utc,
-        }
+        )
       end
     end
 
